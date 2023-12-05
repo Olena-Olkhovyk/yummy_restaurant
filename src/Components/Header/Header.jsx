6d324421af3886f4714f9dc07d3dc21650145758
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-12-05 16:26:13 +0100

    Style header for desktop version

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,24 +1,23 @@
 import classes from "./Header.module.css";
 import logo from "../../assets/logo.png";
-import { TfiAlignJustify } from "react-icons/tfi";
 import { FaShoppingCart } from "react-icons/fa";
 
 const Header = () => {
   return (
     <div className={classes.headerContainer}>
-      <TfiAlignJustify />
-      <ul>
+      <i className="fa-solid fa-bars log"></i>
+      <ul className={classes.list}>
         <li>Харків</li>
         <li>Ресторани</li>
         <li>Контакти</li>
       </ul>
-      <button>-50% на заберу сам</button>
-      <img src={logo} alt="restaurant's logo" />
-      <button>Відстежити замовлення</button>
-      <ul>
+      <button className={classes.saleBtn}>-50% на заберу сам</button>
+      <img src={logo} className={classes.logo} alt="restaurant's logo" />
+      <button className={classes.trackOrder}>Відстежити замовлення</button>
+      <ul className={classes.list}>
         <li>UA</li>
         <li>Вхід</li>
-        <li>
+        <li className={classes.cart}>
           <FaShoppingCart />
           Кошик
         </li>
