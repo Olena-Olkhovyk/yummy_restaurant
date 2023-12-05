c1f5c1593d42729f4e572be7ce96d3cc31dfbcb2
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-12-05 23:33:35 +0100

    Expand header component

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,25 +1,43 @@
 import classes from "./Header.module.css";
 import logo from "../../assets/logo.png";
-import { FaShoppingCart } from "react-icons/fa";
 
 const Header = () => {
   return (
     <div className={classes.headerContainer}>
-      <i className="fa-solid fa-bars log"></i>
-      <ul className={classes.list}>
+      <div className={classes.burger}>
+        <i className="fa-solid fa-bars"></i>
+      </div>
+      <img src={logo} className={classes.logo} alt="restaurant's logo" />
+      <ul className={classes.list1}>
         <li>Харків</li>
         <li>Ресторани</li>
         <li>Контакти</li>
       </ul>
       <button className={classes.saleBtn}>-50% на заберу сам</button>
-      <img src={logo} className={classes.logo} alt="restaurant's logo" />
+
       <button className={classes.trackOrder}>Відстежити замовлення</button>
-      <ul className={classes.list}>
+      <ul className={classes.list2}>
+        <li>
+          <i class="fa-solid fa-location-dot"></i>Харків
+        </li>
+        <li>
+          <i class="fa-solid fa-phone"></i>
+        </li>
         <li>UA</li>
         <li>Вхід</li>
+        <li>
+          <i class="fa-regular fa-user"></i>
+        </li>
         <li className={classes.cart}>
-          <FaShoppingCart />
-          Кошик
+          <div className={classes.desktopContent}>
+            <i class="fa-solid fa-cart-shopping"></i>
+            Кошик
+          </div>
+        </li>
+        <li>
+          <div className={classes.mobileContent}>
+            <i className="fa-solid fa-cart-shopping"></i>
+          </div>
         </li>
       </ul>
     </div>
