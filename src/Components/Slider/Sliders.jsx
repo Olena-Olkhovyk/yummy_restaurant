a3b24ec7e54bcb54819e0640aa18d0318d3ba3a5
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-12-06 22:01:21 +0100

    Delete useRef from input

--- src/Components/Slider/Sliders.jsx
+++ src/Components/Slider/Sliders.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React from "react";
 import Slider from "react-slick";
 import "bootstrap/dist/css/bootstrap.min.css";
 import classes from "./Slider.module.css";
