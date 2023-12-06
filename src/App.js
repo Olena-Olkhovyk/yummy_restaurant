33719df083a9b83b67ac174729177141af3359e0
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-12-06 14:57:39 +0100

    Include sliders component to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Menu from "./Components/Menu/Menu";
+import Sliders from "./Components/Slider/Sliders";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Menu />
+      <Sliders />
     </div>
   );
 }
