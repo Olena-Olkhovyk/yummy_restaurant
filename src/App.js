fa74e06d7de9109a882d8b3404dee1278dcb0f5f
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-12-06 10:52:02 +0100

    Link menu component to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
+import Menu from "./Components/Menu/Menu";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Menu />
     </div>
   );
 }
