19233cd404dafff4cd03fa41035ebed112ca2ffe
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-12-07 10:31:09 +0100

    Add items cards and slider

---
+++ src/Components/Items/Items.jsx
@@ -0,0 +1,61 @@
+import classes from "./Items.module.css";
+import React from "react";
+import Slider from "react-slick";
+import bigpizza from "../../assets/Items/bigpizza.jpg";
+import salad from "../../assets/Items/salad.jpg";
+import chickenSalad from "../../assets/Items/chickenSalad.jpg";
+import burger from "../../assets/Items/burger.jpg";
+const Items = () => {
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
+  const items = [
+    {
+      image: bigpizza,
+      name: "Піцца мафія",
+      price: 299,
+    },
+    {
+      image: salad,
+      name: "Салат цезарь",
+      price: 100,
+    },
+    {
+      image: chickenSalad,
+      name: "Салат із куркою",
+      price: 80,
+    },
+    {
+      image: burger,
+      name: "Бургер",
+      price: 90,
+    },
+  ];
+  return (
+    <div className={classes.itemsContainer}>
+      <h1>Ми рекомендуємо</h1>
+      <div className={classes.itemsCard}>
+        <Slider {...settings}>
+          {items.map(({ image, name, price }) => (
+            <div className={classes.itemCard} key={name}>
+              <img
+                src={image}
+                alt={name}
+                className={`img-fluid ${classes.itemImg}`}
+              />
+              <h1 className={classes.itemName}>{name}</h1>
+              <h3 className={classes.price}>{price} грн</h3>
+              <button className={classes.orderButton}>Замовити</button>
+            </div>
+          ))}
+        </Slider>
+      </div>
+    </div>
+  );
+};
+export default Items;
