cb5562737bedf95fb5f8624bbe15e31205cb604a
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-12-07 10:31:49 +0100

    Add items component to App

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import "./App.css";
 import Header from "./Components/Header/Header";
 import Menu from "./Components/Menu/Menu";
 import Sliders from "./Components/Slider/Sliders";
-
+import Items from "./Components/Items/Items";
 function App() {
   return (
     <div className="App">
       <Header />
       <Menu />
       <Sliders />
+      <Items />
     </div>
   );
 }
