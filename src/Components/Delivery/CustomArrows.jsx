32c2f05f0376decb1fd0a2043d7c61c24cd79d00
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-12-07 13:27:21 +0100

    Add custom arrows component

---
+++ src/Components/Delivery/CustomArrows.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+export const SampleNextArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        position: "absolute",
+        top: "50px",
+        right: "-10px",
+        background: "#8c9f3f",
+        borderRadius: "50%",
+        width: "20px",
+        height: "20px",
+        cursor: "pointer",
+        boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.3)",
+      }}
+      onClick={onClick}
+    />
+  );
+};
+
+export const SamplePrevArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        position: "absolute",
+        top: "50px",
+        left: "-10px",
+        background: "#8c9f3f",
+        borderRadius: "50%",
+        width: "20px",
+        height: "20px",
+        cursor: "pointer",
+        boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.3)",
+      }}
+      onClick={onClick}
+    />
+  );
+};
