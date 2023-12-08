0727f4f1808ce37910c4cbb066a434fa076b610d
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-12-08 14:47:55 +0100

    Create a home component for dynamic content

---
+++ src/Components/Main/Home.jsx
@@ -0,0 +1,13 @@
+import Sliders from "../Slider/Sliders";
+import Items from "../Items/Items";
+import Delivery from "../Delivery/Delivery";
+const Home = () => {
+  return (
+    <>
+      <Sliders />
+      <Items />
+      <Delivery />
+    </>
+  );
+};
+export default Home;
