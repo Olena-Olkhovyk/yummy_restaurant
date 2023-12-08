78e89246e1655ee38632e9d92e29b85221999ac5
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-12-08 14:48:50 +0100

    Made routing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+// import reportWebVitals from "./reportWebVitals";
+import Home from "./Components/Main/Home";
+import ItemDetails from "./Components/Items/ItemDetails";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        element: <Home />,
+        index: true,
+      },
+      {
+        path: "/item/:itemName",
+        element: <ItemDetails />,
+      },
+    ],
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 );
 
-reportWebVitals();
+// reportWebVitals();
