6d8a9823ce6b1179b172622d1a58816b20f7e288
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-12-08 14:48:33 +0100

    Delete dynamic components from App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Menu from "./Components/Menu/Menu";
-import Sliders from "./Components/Slider/Sliders";
-import Items from "./Components/Items/Items";
-import Delivery from "./Components/Delivery/Delivery";
 import Footer from "./Components/Footer/Footer";
+import { Outlet } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Menu />
-      <Sliders />
-      <Items />
-      <Delivery />
-      <Footer />
-    </div>
+    <>
+      <div className="App">
+        <Header />
+        <Menu />
+        <Footer />
+      </div>
+      <Outlet></Outlet>
+    </>
   );
 }
 
