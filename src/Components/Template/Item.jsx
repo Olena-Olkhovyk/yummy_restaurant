639e5ecb4214add73960031adb3a9e798e34ecf4
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-12-12 11:02:31 +0100

    Add className to price and title to style it

--- src/Components/Template/Item.jsx
+++ src/Components/Template/Item.jsx
@@ -4,11 +4,11 @@ const Item = ({ name, image, gramm, liter, price }) => {
   return (
     <div className={classes.itemContainer}>
       <img src={image} alt={name} className={classes.img} />
-      <h2>
+      <h2 className={classes.title}>
         {name}({gramm ? gramm : liter}
         {gramm ? "g" : "l"})
       </h2>
-      <p> {price}grn</p>
+      <p className={classes.price}> {price}grn</p>
       <button>Add to cart</button>
     </div>
   );
