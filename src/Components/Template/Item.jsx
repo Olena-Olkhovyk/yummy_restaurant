dd1fc78c11c005305b0733dc3d866aa41008fdd2
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-12-12 14:33:02 +0100

    Dispatch setItem action on click and link to item details page

--- src/Components/Template/Item.jsx
+++ src/Components/Template/Item.jsx
@@ -1,14 +1,22 @@
 import classes from "./Item.module.css";
-
+import { setItem } from "../../redux/slices/itemSlice";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 const Item = ({ name, image, gramm, liter, price }) => {
+  const dispatch = useDispatch();
   return (
-    <div className={classes.itemContainer}>
-      <img src={image} alt={name} className={classes.img} />
-      <h2 className={classes.title}>
-        {name}({gramm ? gramm : liter}
-        {gramm ? "g" : "l"})
-      </h2>
-      <p className={classes.price}> {price}grn</p>
+    <div
+      className={classes.itemContainer}
+      onClick={() => dispatch(setItem({ name, image, price }))}
+    >
+      <Link to={`/item/${name}`}>
+        <img src={image} alt={name} className={classes.img} />
+        <h2 className={classes.title}>
+          {name}({gramm ? gramm : liter}
+          {gramm ? "g" : "l"})
+        </h2>
+        <p className={classes.price}> {price}grn</p>
+      </Link>
       <button>Add to cart</button>
     </div>
   );
