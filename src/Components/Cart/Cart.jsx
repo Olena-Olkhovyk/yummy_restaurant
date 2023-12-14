ad3780db1334c6886afa60d69ab69fbeb7b1aec3
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-12-14 10:30:26 +0100

    Add cart component

---
+++ src/Components/Cart/Cart.jsx
@@ -0,0 +1,7 @@
+import { useSelector } from "react-redux";
+import classes from "./Cart.module.css";
+import { selectCart } from "../../redux/slices/cartSlice";
+const Cart = () => {
+  return <h1>cart</h1>;
+};
+export default Cart;
