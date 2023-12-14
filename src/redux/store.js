50393066c5b451e4463b4b139ada1c7f3fe73dc0
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-12-14 10:24:39 +0100

    Add cartReducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import itemsReducer from "../redux/slices/itemSlice";
+import cartReducer from "./slices/cartSlice";
 export const store = configureStore({
   reducer: {
     items: itemsReducer,
+    cart: cartReducer,
   },
 });
