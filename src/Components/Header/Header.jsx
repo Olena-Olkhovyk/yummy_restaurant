b00f8b07b54ae4d63253ff657f2944593fe5c3b7
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-12-14 11:21:55 +0100

    Refactor and update styling of cart

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -11,37 +11,19 @@ const Header = () => {
       <Link to="/">
         <img src={logo} className={classes.logo} alt="restaurant's logo" />
       </Link>
-      <ul className={classes.list1}>
-        <li>Харків</li>
-        <li>Ресторани</li>
-        <li>Контакти</li>
-      </ul>
       <button className={classes.saleBtn}>-50% на заберу сам</button>
       <button className={classes.trackOrder}>Відстежити замовлення</button>
       <ul className={classes.list2}>
-        <li>
-          <i class="fa-solid fa-location-dot"></i>
-          <span className={classes.city}>Харків</span>
-        </li>
-        <li>
-          <i class="fa-solid fa-phone"></i>
-        </li>
         <li className={classes.entrance}>Вхід</li>
         <li>
           <i class="fa-regular fa-user"></i>
         </li>
-        <li className={classes.cart}>
-          <div className={classes.desktopContent}>
-            <i class="fa-solid fa-cart-shopping"></i>
-            Кошик
-          </div>
-        </li>
+        <Link to="/cart">
+          <li className={classes.cart}>
+            <i class="fa-solid fa-cart-shopping"></i>Кошик
+          </li>
+        </Link>
         <span className={classes.itemsAmount}>0</span>
-        <li>
-          <div className={classes.mobileContent}>
-            <i className="fa-solid fa-cart-shopping"></i>
-          </div>
-        </li>
       </ul>
     </div>
   );
