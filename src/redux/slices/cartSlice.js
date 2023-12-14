d9d579891b8a054a49c368c55ac6788798bc1c8b
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-12-14 10:24:16 +0100

    Add cartSlice in redux

---
+++ src/redux/slices/cartSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+  },
+  reducers: {
+    addToCart: (state, action) => {
+      const findItem = state.items.find((obj) => obj.id === action.payload.id);
+      if (findItem) {
+        findItem.count++;
+      } else {
+        state.items.push({
+          ...action.payload,
+          count: 1,
+        });
+      }
+    },
+  },
+});
+export const selectCart = (state) => state.cart;
+export const { addToCart } = cartSlice.actions;
+export default cartSlice.reducer;
