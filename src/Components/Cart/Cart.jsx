99780bcaa6e9a110f8444ff11d03656a4a11d7d4
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-12-15 18:59:15 +0100

    Separate the component into subcomponents

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,37 +1,18 @@
 import { useSelector } from "react-redux";
 import classes from "./Cart.module.css";
 import { selectCart } from "../../redux/slices/cartSlice";
+import CartItem from "./CartItem";
 const Cart = ({ isOpen }) => {
   const { items, totalPrice } = useSelector(selectCart);
+
   return (
     <>
       {items.length > 0 && (
         <div className={`${classes.cartBlock} ${isOpen ? classes.show : ""}`}>
           <h2 className={classes.cartTitle}>CART</h2>
           <div className={classes.itemsContainer}>
-            {items.map(({ id, image, name, gramm, liter, price }) => (
-              <div key={id} className={classes.cartItems}>
-                <img src={image} alt={name} className={classes.cartImage} />
-                <div>
-                  <h4 className={classes.itemTitle}>
-                    {name}({gramm ? gramm : liter}
-                    {gramm ? "g" : "L"})
-                  </h4>
-                  <div className={classes.deleteItem}>
-                    <i class="fa-solid fa-xmark"></i>
-                  </div>
-                  <div className={classes.itemAmountContainer}>
-                    <div className={classes.minus}>
-                      <i class="fa-solid fa-minus"></i>
-                    </div>
-                    <span className={classes.itemAmount}>1</span>
-                    <div className={classes.plus}>
-                      <i class="fa-solid fa-plus"></i>
-                    </div>
-                    <h5 className={classes.price}>{price}</h5>
-                  </div>
-                </div>
-              </div>
+            {items.map((item) => (
+              <CartItem key={item.id} {...item} />
             ))}
           </div>
           <div className={classes.orderPrice}>
