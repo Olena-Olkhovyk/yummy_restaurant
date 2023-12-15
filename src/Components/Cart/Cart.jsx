87a22fb194b427b98637427b7794df4e7bda9cfe
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-12-15 14:18:58 +0100

    Prevent opening the cart when it is empty

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -4,28 +4,32 @@ import { selectCart } from "../../redux/slices/cartSlice";
 const Cart = ({ isOpen }) => {
   const { items, totalPrice } = useSelector(selectCart);
   return (
-    <div className={`${classes.cartBlock} ${isOpen ? classes.show : ""}`}>
-      <h1 className={classes.cartTitle}>CART</h1>
-      <div className={classes.itemsContainer}>
-        {items.map(({ id, image, name, gramm, liter, price }) => (
-          <div key={id} className={classes.cartItems}>
-            <img src={image} alt={name} className={classes.cartImage} />
-            <div>
-              <h4>
-                {name}({gramm ? gramm : liter}
-                {gramm ? "g" : "L"})
-              </h4>
-              <p className={classes.price}>{price}</p>
-            </div>
+    <>
+      {items.length > 0 && (
+        <div className={`${classes.cartBlock} ${isOpen ? classes.show : ""}`}>
+          <h2 className={classes.cartTitle}>CART</h2>
+          <div className={classes.itemsContainer}>
+            {items.map(({ id, image, name, gramm, liter, price }) => (
+              <div key={id} className={classes.cartItems}>
+                <img src={image} alt={name} className={classes.cartImage} />
+                <div>
+                  <h4 className={classes.itemTitle}>
+                    {name}({gramm ? gramm : liter}
+                    {gramm ? "g" : "L"})
+                  </h4>
+                  <p className={classes.price}>{price}</p>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-      <div className={classes.orderPrice}>
-        <p>Price:</p>
-        <p>{totalPrice}</p>
-      </div>
-      <button className={classes.btn}>Order</button>
-    </div>
+          <div className={classes.orderPrice}>
+            <p>Price:</p>
+            <p>{totalPrice}</p>
+          </div>
+          <button className={classes.btn}>Order</button>
+        </div>
+      )}
+    </>
   );
 };
 export default Cart;
