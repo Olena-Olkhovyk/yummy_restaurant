4c2cc627b7067b6252dfa4fa9b29ca84ae327ee7
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-12-15 18:54:21 +0100

    Add decrease item functionality to cartSlice

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -21,8 +21,23 @@ const cartSlice = createSlice({
         return item.price * item.count + sum;
       }, 0);
     },
+    decreaseItemCount: (state, action) => {
+      const findItem = state.items.find((obj) => obj.id === action.payload);
+      if (findItem && findItem.count > 1) {
+        findItem.count--;
+        state.totalPrice = state.items.reduce((sum, item) => {
+          return item.price * item.count + sum;
+        }, 0);
+      }
+    },
+    removeItem(state, action) {
+      state.items = state.items.filter((obj) => obj.id !== action.payload);
+      state.totalPrice = state.items.reduce((sum, obj) => {
+        return obj.price * obj.count + sum;
+      }, 0);
+    },
   },
 });
 export const selectCart = (state) => state.cart;
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, decreaseItemCount, removeItem } = cartSlice.actions;
 export default cartSlice.reducer;
