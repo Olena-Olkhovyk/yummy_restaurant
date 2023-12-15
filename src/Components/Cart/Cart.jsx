5493b8bfb90faae6280588342a7f60846210e280
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-12-15 13:03:52 +0100

    Display item's total price in cart

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -2,23 +2,28 @@ import { useSelector } from "react-redux";
 import classes from "./Cart.module.css";
 import { selectCart } from "../../redux/slices/cartSlice";
 const Cart = ({ isOpen }) => {
-  const { items } = useSelector(selectCart);
+  const { items, totalPrice } = useSelector(selectCart);
   return (
     <div className={`${classes.cartBlock} ${isOpen ? classes.show : ""}`}>
       <h1 className={classes.cartTitle}>CART</h1>
-      {items.map(({ id, image, name, gramm, liter, price }) => (
-        <div key={id} className={classes.cartItems}>
-          <img src={image} alt={name} className={classes.cartImage} />
-          <div>
-            <h4>
-              {name}({gramm ? gramm : liter}
-              {gramm ? "g" : "L"})
-            </h4>
-            <p className={classes.price}>{price}</p>
-            <hr />
+      <div className={classes.itemsContainer}>
+        {items.map(({ id, image, name, gramm, liter, price }) => (
+          <div key={id} className={classes.cartItems}>
+            <img src={image} alt={name} className={classes.cartImage} />
+            <div>
+              <h4>
+                {name}({gramm ? gramm : liter}
+                {gramm ? "g" : "L"})
+              </h4>
+              <p className={classes.price}>{price}</p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
+      <div className={classes.orderPrice}>
+        <p>Price:</p>
+        <p>{totalPrice}</p>
+      </div>
       <button className={classes.btn}>Order</button>
     </div>
   );
