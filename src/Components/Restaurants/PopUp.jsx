762662d8dbf1e60bf2051b8e9cc9467796cfb847
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-12-21 15:14:41 +0100

    Change sortList name

--- src/Components/Restaurants/PopUp.jsx
+++ src/Components/Restaurants/PopUp.jsx
@@ -1,28 +1,31 @@
 import classes from "./Restaurants.module.css";
-import React, { useRef } from "react";
-import useOutsideClick from "../useOutsideClick";
+import { useRef, useState } from "react";
+import useOutsideClick from "../../hooks/useOutsideClick";
+import { restaurants } from "../../assets/Data/restaurants";
 
 const PopUp = () => {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
   const ref = useRef();
-  const sortList = ["Kharkiv", "Kyiv", "Dnipro"];
+  const cities = ["Kharkiv", "Kyiv", "Dnipro"];
 
   useOutsideClick(ref, () => {
     if (open) setOpen(false);
   });
   return (
-    <div className={classes.popUpContainer} ref={ref}>
-      <h3 onClick={() => setOpen(!open)}>Kyiv</h3>
-      <div className={classes.sortPopUp}>
-        {open && (
-          <ul>
-            {sortList.map((list, i) => (
-              <li key={i}>{list}</li>
-            ))}
-          </ul>
-        )}
+    <>
+      <div className={classes.popUpContainer} ref={ref}>
+        <h3 onClick={() => setOpen(!open)}>Kyiv</h3>
+        <div className={classes.sortPopUp}>
+          {open && (
+            <ul>
+              {cities.map((city, i) => (
+                <li key={i}>{city}</li>
+              ))}
+            </ul>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 export default PopUp;
