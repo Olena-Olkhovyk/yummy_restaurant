56af64aec1201ee0f769e04427900858b3446d25
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-12-21 10:56:51 +0100

    Add popup to restaurant component

--- src/Components/Restaurants/Restaurants.jsx
+++ src/Components/Restaurants/Restaurants.jsx
@@ -3,12 +3,23 @@ import Map from "./Map";
 import classes from "./Restaurants.module.css";
 const Restaurants = () => {
   const [open, setOpen] = React.useState(false);
-  const [activeInd, setActiveInd] = React.useState(0);
   const sortList = ["Kharkiv", "Kyiv", "Dnipro"];
   return (
     <div className={classes.restaurantsContainer}>
       <h1 className={classes.title}>Restaurants</h1>
       <Map />
+      <div className={classes.popUpContainer}>
+        <h3 onClick={() => setOpen(!open)}>Kyiv</h3>
+        <div className={classes.sortPopUp}>
+          {open && (
+            <ul>
+              {sortList.map((list, i) => (
+                <li key={i}>{list}</li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
