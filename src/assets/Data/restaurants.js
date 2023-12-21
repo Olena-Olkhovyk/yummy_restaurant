c994eecdbd013539a6589cddfb4440b4089b5eac
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-12-21 15:04:29 +0100

    Add array with restaurants

--- src/assets/Data/restaurants.js
+++ src/assets/Data/restaurants.js
@@ -1,1 +1,91 @@
-export const restaurants = [];
+import cafe1 from "../Restaurants/cafe1.jpg";
+import cafe2 from "../Restaurants/cafe2.jpg";
+import cafe3 from "../Restaurants/cafe3.jpg";
+import cafe4 from "../Restaurants/cafe4.jpeg";
+import cafe5 from "../Restaurants/cafe5.jpg";
+import cafe6 from "../Restaurants/cafe6.jpg";
+import cafe7 from "../Restaurants/cafe7.webp";
+import cafe8 from "../Restaurants/cafe8.webp";
+import cafe9 from "../Restaurants/cafe9.webp";
+import cafe10 from "../Restaurants/cafe10.webp";
+import cafe11 from "../Restaurants/cafe11.webp";
+
+export const restaurants = [
+  {
+    city: "Kyiv",
+    image: cafe1,
+    street: "st. Khreshchatyk, 6",
+    hours: "MON-THU 12:00-22:00 FRI-SUN:12:00-23:00",
+    phone: "(067) 226-35-35",
+  },
+  {
+    city: "Kyiv",
+    image: cafe2,
+    street: "st. Marshal Timoshenko, 21, building 3",
+    hours: "FRI-SUN 11:00-23:00",
+    phone: "(067) 572-17-62",
+  },
+  {
+    city: "Kyiv",
+    image: cafe3,
+    street: "st. Honoré de Balzac, 63",
+    hours: "MON-THU 12:00-22:00 FRI-SUN:12:00-23:00",
+    phone: "(067) 543-57-57",
+  },
+  {
+    city: "Kyiv",
+    image: cafe4,
+    street: "Ave. Beresteysky, 136",
+    hours: "MON-THU 12:00-22:00 FRI-SUN:12:00-23:00",
+    phone: "(067) 226-35-35",
+  },
+  {
+    city: "Kyiv",
+    image: cafe5,
+    street: "st. Bogdan Khmelnitsky, 27/1",
+    hours: "MON-SUN 10:00-23:00",
+    phone: "(067) 403-03-48",
+  },
+  {
+    city: "Kyiv",
+    image: cafe6,
+    street: "st. Kharkivske highway, 144a",
+    hours: "SUN-THU 11:00-22:00 Fri-SAT 11:00-23:00",
+    phone: "(067) 383-51-51",
+  },
+  {
+    city: "Kharkiv",
+    image: cafe7,
+    street: "Kvitky-Osnovyanenko, 7",
+    hours: "10:00 - 22:00",
+    phone: "(073) 435-77-05",
+  },
+  {
+    city: "Kharkiv",
+    image: cafe8,
+    street: "st. Yaroslav the Wise, 37",
+    hours: "10:00 - 22:00",
+    phone: "(099) 212-80-65",
+  },
+  {
+    city: "Kharkiv",
+    image: cafe9,
+    street: "48-G L.Svobody Ave.",
+    hours: "10:00 - 22:00",
+    phone: "(067) 546-43-41",
+  },
+  {
+    city: "Dnipro",
+    image: cafe10,
+    street: "st. Volodymyra Monomakh, 2 k",
+    hours: "10:00 - 22.30",
+    phone: "(097) 278-28-83",
+  },
+  {
+    city: "Dnipro",
+    image: cafe11,
+    street: "st. Nizhnedneprovskaya, 17",
+    hours: "Mon-Fri: from 11:00 to 20:00 Sat-Sun: from 10:00 to 20:00",
+    phone: "(096) 438-32-32",
+  },
+];
