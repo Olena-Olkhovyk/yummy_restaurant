ba1b3bc18e6af9b7a2f89790852e8fc00d5b71f3
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-12-25 10:18:14 +0100

    Add popup to restaurant page

--- src/Components/Restaurants/PopUp.jsx
+++ src/Components/Restaurants/PopUp.jsx
@@ -1,29 +1,37 @@
 import classes from "./Restaurants.module.css";
 import { useRef, useState } from "react";
 import useOutsideClick from "../../hooks/useOutsideClick";
-import { restaurants } from "../../assets/Data/restaurants";
-
+import RestaurantInfo from "./RestaurantInfo";
 const PopUp = () => {
   const [open, setOpen] = useState(false);
+  const [selectedCity, setSelectedCity] = useState("Kyiv");
   const ref = useRef();
   const cities = ["Kharkiv", "Kyiv", "Dnipro"];
-
+  const handleCityClick = (city) => {
+    setSelectedCity(city);
+    setOpen(false);
+  };
   useOutsideClick(ref, () => {
     if (open) setOpen(false);
   });
   return (
     <>
       <div className={classes.popUpContainer} ref={ref}>
-        <h3 onClick={() => setOpen(!open)}>Kyiv</h3>
+        <h3 onClick={() => setOpen(!open)}>{selectedCity}</h3>
         <div className={classes.sortPopUp}>
           {open && (
             <ul>
               {cities.map((city, i) => (
-                <li key={i}>{city}</li>
+                <li key={i} onClick={() => handleCityClick(city)}>
+                  {city}
+                </li>
               ))}
             </ul>
           )}
         </div>
+      </div>
+      <div className={classes.restaurantsInfo}>
+        <RestaurantInfo selectedCity={selectedCity} />
       </div>
     </>
   );
